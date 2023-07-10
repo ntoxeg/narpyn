09bfdee4c228fac2db0e90c976edb3dd7ab6c306
Adrian Borucki <ab@synthillect.ai>
2023-07-10 14:07:37 +0200

    Fix types

--- narpyn/ona/NAR/__init__.py
+++ narpyn/ona/NAR/__init__.py
@@ -2,12 +2,11 @@
 import os
 import subprocess
 import sys
-from numbers import Number
 
 NARPKG_PATH = os.path.dirname(__file__)
 
 
-def _program(name: str, args: list[str | Number]) -> int:
+def _program(name: str, args: list[str]) -> int:
     return subprocess.call([os.path.join(NARPKG_PATH, name)] + args, close_fds=False)
 
 
